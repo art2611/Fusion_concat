954d9ca11027ecbd16c91dbdb75a44396f6e043e
art2611 <art261197@hotmail.fr>
2021-01-18 00:16:51 +0100

    commit

--- essais.py
+++ essais.py
@@ -1,10 +1,19 @@
 import numpy as np
-
+import torch
+import sys
 import matplotlib
 import matplotlib.pyplot as plt
 from scipy.spatial.distance import pdist
 
+x = torch.randn(2, 3)
+print(x)
+cat = torch.cat((x,x), 1)
+print(cat)
+print(cat.shape)
+print(x.shape)
 
+
+sys.exit()
 a = [[2,0], [4,6],[0,1]]
 a = np.array(a)
 print(a)
