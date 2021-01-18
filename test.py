9d1365f2d24208a3528ea198cfb6ddb20fc8ec59
art2611 <art261197@hotmail.fr>
2021-01-18 21:05:21 +0100

    commit

--- test.py
+++ test.py
@@ -40,7 +40,6 @@ parser.add_argument('--fusion', default='layer1', help='Layer to fuse data')
 parser.add_argument('--dataset', default='regdb', help='dataset name: regdb or sysu]')
 parser.add_argument('--reid', default='VtoT', help='Visible to thermal reid')
 parser.add_argument('--trained', default='VtoT', help='Model trained based on VtoT validation')
-parser.add_argument('--fold', default='0', help='Fold number, from 0 to 4')
 parser.add_argument('--split', default='paper_based', help='How to split data')
 args = parser.parse_args()
 
