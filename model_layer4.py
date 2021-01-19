882305a3a6df935bcc3893a6a1dee2183c2c53b5
art2611 <art261197@hotmail.fr>
2021-01-19 12:09:28 +0100

    commit

--- model_layer4.py
+++ model_layer4.py
@@ -34,7 +34,9 @@ class visible_module(nn.Module):
         x = self.visible.bn1(x)
         x = self.visible.relu(x)
         x = self.visible.maxpool(x)
-
+        x = self.visible.layer1(x)
+        x = self.visible.layer2(x)
+        x = self.visible.layer3(x)
         return x
 
 class thermal_module(nn.Module):
@@ -50,6 +52,9 @@ class thermal_module(nn.Module):
         x = self.thermal.bn1(x)
         x = self.thermal.relu(x)
         x = self.thermal.maxpool(x)
+        x = self.thermal.layer1(x)
+        x = self.thermal.layer2(x)
+        x = self.thermal.layer3(x)
         return x
 
 class shared_resnet(nn.Module):
@@ -63,9 +68,6 @@ class shared_resnet(nn.Module):
         self.base = model_base
 
     def forward(self, x):
-        x = self.base.layer1(x)
-        x = self.base.layer2(x)
-        x = self.base.layer3(x)
         x = self.base.layer4(x)
         return x
 
