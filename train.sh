12eae5c5edaa2f031f954ca9874125d3de1c0a36
art2611 <art261197@hotmail.fr>
2021-01-20 18:57:32 +0100

    commit

--- train.sh
+++ train.sh
@@ -7,9 +7,13 @@ read -e -p "Enter the dataset name (sysu/regdb) :" DATASET
 echo $DATASET
 read -e -p "Enter the the fuse type (sum/cat/none) :" FUSE
 echo $FUSE
-
+echo \############################### START FOLD 1 \###############################
 python train.py --fusion=$FUSION --dataset=$DATASET --reid=$REID --fuse=$FUSE --fold=0;
+echo \############################### START FOLD 2 \###############################
 python train.py --fusion=$FUSION --dataset=$DATASET --reid=$REID --fuse=$FUSE --fold=1;
+echo \############################### START FOLD 3 \###############################
 python train.py --fusion=$FUSION --dataset=$DATASET --reid=$REID --fuse=$FUSE --fold=2;
+echo \############################### START FOLD 4 \###############################
 python train.py --fusion=$FUSION --dataset=$DATASET --reid=$REID --fuse=$FUSE --fold=3;
+echo \############################### START FOLD 5 \###############################
 python train.py --fusion=$FUSION --dataset=$DATASET --reid=$REID --fuse=$FUSE --fold=4;
