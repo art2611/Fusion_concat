fdebaeeefb2d2d6560bee87f38607cf837083c7a
art2611 <art261197@hotmail.fr>
2021-01-24 15:28:59 +0100

    commit

--- train.sh
+++ train.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 read -e -p "Enter the fusion type (early/layerX/unimodal) : " FUSION
 echo $FUSION
-read -e -p "Enter the reid type (BtoB) : " REID
-echo $REID
+if [ "$FUSION" =  "unimodal" ]
+then
+    read -e -p "Enter the wanted reid type (VtoV / TtoT) : " REID
+    echo $VAR
+else
+    REID="BtoB"
+fi
 read -e -p "Enter the dataset name (sysu/regdb) :" DATASET
 echo $DATASET
 read -e -p "Enter the the fuse type (sum/cat/none) :" FUSE
