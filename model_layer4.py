9b89c63c072474ae7b16873bb1b12d504cae1d27
art2611 <art261197@hotmail.fr>
2021-01-26 09:59:53 +0100

    commit

--- model_layer4.py
+++ model_layer4.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 from torch.nn import init
 from torchvision.models import resnet50
-from torchsummary import summary
+# from torchsummary import summary
 
 class Identity(nn.Module):
     def __init__(self):
