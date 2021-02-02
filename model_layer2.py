247e9d6091fcf6f58438313d67ceff390b2586a8
art2611 <art261197@hotmail.fr>
2021-02-02 14:44:02 +0100

    git pu

--- model_layer2.py
+++ model_layer2.py
@@ -93,7 +93,7 @@ class Network_layer2(nn.Module):
         self.thermal_module = thermal_module(arch=arch)
         self.visible_module = visible_module(arch=arch)
         self.convolution_after_fuse = torch.nn.Conv2d(512, 256,1)
-        self.fusion_function_concat = fusion_function_concat()
+        # self.fusion_function_concat = fusion_function_concat()
         self.shared_resnet = shared_resnet(arch=arch)
 
         pool_dim = 2048
@@ -116,9 +116,9 @@ class Network_layer2(nn.Module):
             elif fuse == "sum":
                 x = x1.add(x2)
             elif fuse == "cat_channel" :
-                x = self.fusion_function_concat(x1, x2)
-                # x = torch.cat((x1, x2), 1)
-                # x = self.convolution_after_fuse(x)
+                # x = self.fusion_function_concat(x1, x2)
+                x = torch.cat((x1, x2), 1)
+                x = self.convolution_after_fuse(x)
         elif modal == 1:
             x = self.visible_module(x1)
         elif modal == 2:
