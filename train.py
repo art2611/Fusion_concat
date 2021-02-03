827e23f2a28455b10457553a56fff03903d3b884
art2611 <art261197@hotmail.fr>
2021-02-03 18:55:26 +0100

    git pull

--- train.py
+++ train.py
@@ -440,6 +440,7 @@ for epoch in range(epoch_number):
         #         cmc_att[0], cmc_att[4], cmc_att[9], cmc_att[19], mAP_att, mINP_att))
         print('Rank-1: {:.2%} | Rank-5: {:.2%} | mAP: {:.2%}| mINP: {:.2%}'.format(cmc_att[0], cmc_att[4], cmc_att[9], cmc_att[19], mAP_att, mINP_att))
         print('Best Epoch [{}]'.format(best_epoch))
+        print(f' Training {args.fusion} {args.fuse} fusion')
 
-print(f' Training time for {args.fusion} fusion : {time.time() - training_time}')
+print(f' Training time for {args.fusion} {args.fuse} fusion : {time.time() - training_time}')
 
