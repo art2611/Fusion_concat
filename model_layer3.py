fcfe54789f7c165f13ae3602e5e3930937b95599
art2611 <art261197@hotmail.fr>
2021-02-03 11:11:44 +0100

    git pull

--- model_layer3.py
+++ model_layer3.py
@@ -78,7 +78,7 @@ class fusion_function_concat(nn.Module):
         layers = [
             # nn.Conv2d(1024, 512, kernel_size=7, stride=2, padding=3,bias=False),
             nn.Conv2d(512, 256, kernel_size=7, stride=2, padding=3,bias=False),
-            nn.BatchNorm2d(512),
+            nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         ]
