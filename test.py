c78f7b686eb64ffce3441d09450d93e4f42665a6
art2611 <art261197@hotmail.fr>
2021-02-07 12:57:54 +0100

    git pull

--- test.py
+++ test.py
@@ -346,7 +346,7 @@ if args.dataset == 'SYSU':
             if args.fusion == "score" :
                 # Proceed to 2nd matching and aggregate matching matrix
                 distmat2 = np.matmul(query_feat_fc2, np.transpose(gall_feat_fc2))
-                distmat = distmat + distmat2
+                distmat = 0.7*distmat + distmat2
             else :
                 # Proceed to a simple feature aggregation, features incoming from two distinct unimodal trained models
                 query_feat_fc = query_feat_fc + query_feat_fc2
