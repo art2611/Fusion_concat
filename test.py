5c2b73fc181c7d6fb2a638dffcbb8f88308fd2cc
art2611 <art261197@hotmail.fr>
2021-02-17 10:26:28 +0100

    git pull

--- test.py
+++ test.py
@@ -352,7 +352,9 @@ if args.dataset == 'SYSU':
             else :
                 # Proceed to a simple feature aggregation, features incoming from the two distinct unimodal trained models (RGB and IR )
                 #First do a minmax norm :
+                print(query_final_fc[0])
                 query_final_fc = minmax_norm(query_final_fc)
+                print(query_final_fc[0])
                 query_final_fc2 = minmax_norm(query_final_fc2)
                 gall_final_fc = minmax_norm(gall_final_fc)
                 gall_final_fc2 = minmax_norm(gall_final_fc2)
