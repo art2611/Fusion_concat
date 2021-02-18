b2948244c0ec48506282252d0708f28884135f4f
art2611 <art261197@hotmail.fr>
2021-02-18 15:33:54 +0100

    git pull

--- test.py
+++ test.py
@@ -251,11 +251,11 @@ if args.dataset == "RegDB":
                 # Proceed to a simple feature aggregation, features incoming from the two distinct unimodal trained models (RGB and IR )
                 #First do a minmax norm :
                 # print(query_final_fc[0])
-                query_final_fc = Z_mean(query_final_fc)
+                query_final_fc = minmax_norm(query_final_fc)
                 # print(query_final_fc[0])
-                query_final_fc2 = Z_mean(query_final_fc2)
-                gall_final_fc = Z_mean(gall_final_fc)
-                gall_final_fc2 = Z_mean(gall_final_fc2)
+                query_final_fc2 = minmax_norm(query_final_fc2)
+                gall_final_fc = minmax_norm(gall_final_fc)
+                gall_final_fc2 = minmax_norm(gall_final_fc2)
 
                 #then aggregate all
                 query_feat_fc = (query_final_fc + query_final_fc2) / 2
