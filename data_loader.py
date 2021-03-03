1b6dcb0f63a7892384c1026069cacdff28685d8b
art2611 <art261197@hotmail.fr>
2021-03-03 22:45:25 +0100

    git pull

--- data_loader.py
+++ data_loader.py
@@ -178,7 +178,6 @@ def process_tworld(img_dir, mode, fold):
             #Since all images are in a same folder, we get all an id here
             new_files = sorted([img_dir + '/' + i for i in os.listdir(img_dir)])
             ids_file_RGB.append(new_files)
-            print(f"new files : {ids_file_RGB}")
 
         img_dir = os.path.join(img_dir, "IR_8", str(id))
         if os.path.isdir(img_dir):
