6d47993f5c370949797260290acf73baee23ea0c
art2611 <art261197@hotmail.fr>
2021-03-06 15:08:45 +0100

    Big change in data use

--- train_all.sh
+++ train_all.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-
 REID="BtoB"
-
 read -e -p "Enter the dataset name (SYSU/RegDB) :" DATASET
 echo $DATASET
 if [ "$DATASET" =  "regdb" ]
@@ -15,25 +13,23 @@ fi
 read -e -p "Enter the the fuse type (sum/cat/cat_channel/none) :" FUSE
 echo $FUSE
 
-# IF YOU WANT TO TRAIN UNIMODAL MODELS :
-
 for i in `seq 1 5`;
 do
         echo "BEGINING OF THE FIRST TRAINING : Unimodal RGB - Fold = $i"
         python train.py --fusion="unimodal" --dataset=$DATASET --reid="VtoV" --fuse="none" --fold=$i;
 done
-
-for i in `seq 1 5`;
-do
-        echo "BEGINING OF THE FIRST TRAINING : Unimodal IR - Fold = $i"
-        python train.py --fusion="unimodal" --dataset=$DATASET --reid="TtoT" --fuse="none" --fold=$i;
-done
-
-for j in 'early' 'layer1' 'layer2' 'layer3' 'layer4' 'layer5';
-do
-  for i in `seq 1 5`;
-  do
-          echo "BEGINING OF THE FIRST TRAINING : $j - Fold = $i"
-          python train.py --fusion=$j --dataset=$DATASET --reid="BtoB" --fuse=$FUSE --fold=$i;
-  done
-done
+#
+#for i in `seq 1 5`;
+#do
+#        echo "BEGINING OF THE FIRST TRAINING : Unimodal IR - Fold = $i"
+#        python train.py --fusion="unimodal" --dataset=$DATASET --reid="TtoT" --fuse="none" --fold=$i;
+#done
+#
+#for j in 'early' 'layer1' 'layer2' 'layer3' 'layer4' 'layer5';
+#do
+#  for i in `seq 1 5`;
+#  do
+#          echo "BEGINING OF THE FIRST TRAINING : $j - Fold = $i"
+#          python train.py --fusion=$j --dataset=$DATASET --reid="BtoB" --fuse=$FUSE --fold=$i;
+#  done
+#done
