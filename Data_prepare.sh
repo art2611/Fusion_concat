e39c61de516d992acf7de271538048933c45f7df
art2611 <art261197@hotmail.fr>
2021-03-06 12:50:28 +0100

    Big change in data use

--- Data_prepare.sh
+++ Data_prepare.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 mkdir ../Datasets/RegDB/exp
 mv dataset_use/SYSU/*.txt ../Datasets/SYSU/exp
-mv dataset_use/TWorld/*.txt ../Datasets/TWorld/exp
+#mv dataset_use/TWorld/*.txt ../Datasets/TWorld/exp
 mv dataset_use/RegDB/*.txt ../Datasets/RegDB/exp
 rm ../Datasets/SYSU/*.npy
 rm ../Datasets/RegDB/*.npy
-rm ../Datasets/TWorld/*.npy
+#rm ../Datasets/TWorld/*.npy
 python pre_process_regdb_clean.py
 python pre_process_sysu_clean.py
-python pre_process_thermalWORLD.py
+#python pre_process_thermalWORLD.py
 rm ../save_model/*
 rm -r results.txt
 rm -r runs
