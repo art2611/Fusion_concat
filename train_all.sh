4bc5c5cb722f97e4ae7f3f8e4f4e15bd2e843c8f
art2611 <art261197@hotmail.fr>
2021-03-06 15:15:24 +0100

    Big change in data use

--- train_all.sh
+++ train_all.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 REID="BtoB"
-read -e -p "Enter the dataset name (SYSU/RegDB) :" DATASET
+read -e -p "Enter the dataset name (SYSU/RegDB/TWorld) :" DATASET
 echo $DATASET
 if [ "$DATASET" =  "regdb" ]
 then
@@ -10,30 +10,30 @@ then
   DATASET="SYSU"
 fi
 
-read -e -p "Enter the the fuse type (sum/cat/cat_channel/none) :" FUSE
-echo $FUSE
-for i in `seq 1 5`;
+#read -e -p "Enter the the fuse type (sum/cat/cat_channel/none) :" FUSE
+#echo $FUSE
+
+
+for i in `seq 0 4`;
 do
-        echo "$i"
+        echo "BEGINING OF THE FIRST TRAINING : Unimodal RGB - Fold = $i"
+        python train.py --fusion="unimodal" --dataset=$DATASET --reid="VtoV" --fuse="none" --fold=$i;
 done
-#python train.py --fusion="unimodal" --dataset=$DATASET --reid="VtoV" --fuse="none" --fold=1;
-#for i in `seq 1 5`;
-#do
-#        echo "BEGINING OF THE FIRST TRAINING : Unimodal RGB - Fold = $i"
-#        python train.py --fusion="unimodal" --dataset=$DATASET --reid="VtoV" --fuse="none" --fold=$i;
-#done
-#
-#for i in `seq 1 5`;
-#do
-#        echo "BEGINING OF THE FIRST TRAINING : Unimodal IR - Fold = $i"
-#        python train.py --fusion="unimodal" --dataset=$DATASET --reid="TtoT" --fuse="none" --fold=$i;
-#done
-#
-#for j in 'early' 'layer1' 'layer2' 'layer3' 'layer4' 'layer5';
-#do
-#  for i in `seq 1 5`;
-#  do
-#          echo "BEGINING OF THE FIRST TRAINING : $j - Fold = $i"
-#          python train.py --fusion=$j --dataset=$DATASET --reid="BtoB" --fuse=$FUSE --fold=$i;
-#  done
-#done
+
+for i in `seq 0 4`;
+do
+        echo "BEGINING OF THE FIRST TRAINING : Unimodal IR - Fold = $i"
+        python train.py --fusion="unimodal" --dataset=$DATASET --reid="TtoT" --fuse="none" --fold=$i;
+done
+
+for w in 'sum' 'cat' 'cat_channel';
+do
+  for j in 'early' 'layer1' 'layer2' 'layer3' 'layer4' 'layer5';
+  do
+    for i in `seq 0 4`;
+      do
+              echo "BEGINING OF THE FIRST TRAINING : $j - Fold = $i fuse = $w"
+              python train.py --fusion=$j --dataset=$DATASET --reid="BtoB" --fuse=$w --fold=$i;
+      done
+  done
+done
