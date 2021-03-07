a95fc69fecdf7ffaf606f338c9c94dd5cab8bf9f
art2611 <art261197@hotmail.fr>
2021-03-07 11:59:30 +0100

    Big change in data use

--- evaluation.py
+++ evaluation.py
@@ -29,7 +29,7 @@ def eval_regdb(distmat, query_labels, gallery_labels, max_rank=20):
         # remove gallery samples that have the same pid and camid with query
         order = indices[q_idx]
         remove = (gallery_labels[order] == q_pid) & (g_camids[order] == q_camid)
-        print(remove)
+        print(len(remove))
         keep = np.invert(remove)
 
         # compute cmc curve
