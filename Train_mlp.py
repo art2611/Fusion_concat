35da3be62e0ba918a0d095fbfb4e83c6a17de60b
art2611 <art261197@hotmail.fr>
2021-03-08 12:38:44 +0100

    Commit

--- Train_mlp.py
+++ Train_mlp.py
@@ -80,7 +80,9 @@ for epochs in range(num_epochs):
 
         output = net(input1, input2)
         print(output)
-        print(labels)
+        for k in range(len(labels)) :
+            labels[k] = [k]
+
         loss = criterion_id(output, labels)
         print(loss)
         optimizer.zero_grad()
