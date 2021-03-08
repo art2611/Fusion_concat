1d610213279043c0ff9383e12c612c342f7e4c91
art2611 <art261197@hotmail.fr>
2021-03-08 11:41:49 +0100

    commit

--- utils.py
+++ utils.py
@@ -57,8 +57,6 @@ class IdentityFeatureSampler(Sampler):
         uni_label = np.unique(train_features_label)
         self.n_classes = len(uni_label)
         N = len(train_features_label)
-        index1 = np.array([int()])
-        index2 = np.array([int()])
         # Doing as much batch as we can divide the dataset in number of batch
         for j in range(int(N / (batch_num_identities * num_of_same_id_in_batch)) + 1):
             # We choose randomly 8 identities
@@ -69,14 +67,16 @@ class IdentityFeatureSampler(Sampler):
                 # We choose randomly num_of_same_id_in_batch=4 concatenated features  for the i(batch_num_identities)=8 identitities
                 if dataset == "Tworld" or dataset == "RegDB":
                     sample_features = np.random.choice(features_pos[batch_idx[i]], num_of_same_id_in_batch)
-                print(sample_features)
-
-                # This way in a batch we compare first feature id with all 4*8 features (Need to verify the type of index 1 and 2 here.
-                index1 = np.hstack((index1, [sample_features[0] for w in range(num_of_same_id_in_batch)]))
-
-                index2 = np.hstack(( index2, sample_features))
-                print(index1)
-                print(index2)
+                if i == 0 and j == 0:
+                    # This way in a batch we compare first feature id with all 4*8 features (Need to verify the type of index 1 and 2 here.
+                    index1 = [sample_features[0] for w in range(num_of_same_id_in_batch)]
+                    index2 = sample_features
+                elif i == 0 :
+                    index1 = np.hstack((index1, [sample_features[0] for w in range(len(sample_features))]))
+                    index2 = np.hstack((index2, sample_features))
+                else :
+                    index1 = np.hstack((index1, [index1[j*32] for w in range(len(sample_features))]))
+                    index2 = np.hstack((index2, sample_features))
 
         print(f"index1 : {index1}")
         print(f"index2 : {index2}")
