aea3edf5e90e0287da973458d2b45f34489482fe
art2611 <art261197@hotmail.fr>
2021-03-08 16:54:45 +0100

    Commit

--- data_loader.py
+++ data_loader.py
@@ -342,11 +342,11 @@ def process_sysu(data_path, method, fold = 0 ):
             # Get one images as query
             if i == 0:
                 img_query.append([files_rgb[positions_list_RGB[fold_or_trial][id][i]], files_ir[positions_list_IR[fold_or_trial][id][i]]])
-                label_query.append(ids[id])
+                label_query.append(str(ids[id]))
             # Get the remaining as gallery :
             else:
                 img_gallery.append([files_rgb[positions_list_RGB[fold_or_trial][id][i]], files_ir[positions_list_IR[fold_or_trial][id][i]]])
-                label_gallery.append(ids[id])
+                label_gallery.append(str(ids[id]))
     # Just give different cam id to not have problem during SYSU evaluation
     gall_cam = [4 for i in range(len(img_gallery))]
     query_cam = [1 for i in range(len(img_query))]
