52acb8a0bef62816e1bcdd9bf0a497adc174571d
art2611 <art261197@hotmail.fr>
2021-03-08 17:06:28 +0100

    commit

--- evaluation.py
+++ evaluation.py
@@ -5,9 +5,7 @@ import sys
 """Cross-Modality ReID"""
 def evaluation(distmat, query_labels, gallery_labels, max_rank=20, dataset = "RegDB"):
     num_q, num_g = distmat.shape
-    print(distmat.shape)
-    print(type(max_rank))
-    print(type(num_q))
+
     if num_g < max_rank:
         max_rank = num_g
         print("Note: number of gallery samples is quite small, got {}".format(num_g))
