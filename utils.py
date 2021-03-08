5148cb70866ad6a03994e651b8ec12dbdc0a6552
art2611 <art261197@hotmail.fr>
2021-03-08 10:45:47 +0100

    Commit

--- utils.py
+++ utils.py
@@ -76,8 +76,8 @@ class IdentityFeatureSampler(Sampler):
                     index1 = np.hstack((index1, [index1[0] for w in range(len(sample_features))]))
                     index2 = np.hstack((index2, sample_features))
 
-        # print(f"index1 : {index1}")
-        # print(f"index2 : {index2}")
+        print(f"index1 : {index1}")
+        print(f"index2 : {index2}")
         self.index1 = index1
         self.index2 = index2
         self.N = N
