2c2fac0969931b02b33f7284f0dfbda401d00f07
art2611 <art261197@hotmail.fr>
2021-03-08 10:29:30 +0100

    Commit

--- Train_mlp.py
+++ Train_mlp.py
@@ -7,7 +7,7 @@ from torch.autograd import Variable
 import time
 from data_loader import *
 from torchvision import transforms
-from utils import IdentitySampler, AverageMeter, adjust_learning_rate
+from utils import IdentityFeatureSampler, AverageMeter, adjust_learning_rate
 from loss import BatchHardTripLoss
 from tensorboardX import SummaryWriter
 from model import Global_network,  MLP
@@ -28,6 +28,7 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 test_batch_size = 64
 batch_num_identities = 8 # 8 different identities in a batch
 num_of_same_id_in_batch = 4 # Number of same identity in a batch
+loader_batch = batch_num_identities * num_of_same_id_in_batch
 workers = 4
 lr = 0.1
 
@@ -40,4 +41,33 @@ feature_size = len(trainset.train_features[0])
 print(feature_size)
 net = MLP().to(device)
 print(net(trainset.train_features[0], trainset.train_features[1]))
+
+print('==> Preparing Data Loader...')
+# identity sampler - Give iteratively index from a randomized list of color index and thermal index
+sampler = IdentityFeatureSampler(trainset.train_label_features, feature_pos, num_of_same_id_in_batch, batch_num_identities, args.dataset)
+
+trainset.cIndex = sampler.index1  # color index
+trainset.tIndex = sampler.index2  # thermal index
+# print(epoch)
+# print(trainset.cIndex)
+# print(trainset.tIndex)
+
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=loader_batch, \
+                                          sampler=sampler, num_workers=workers, drop_last=True)
+# print(len(trainloader))
+
+# training
+for batch_idx, (input1, input2, label1, label2) in enumerate(trainloader):
+    # Labels 1 and 2 could be the same or not. If not : label = 0 If yes : label =  1
+    print(label1)
+    print(label2)
+    # if label1 == label2 :
+    #     labels = 1
+    # else :
+    #     labels = 2
+    # print(labels)
+    #
+    # input1 = Variable(input1.cuda())
+    # input2 = Variable(input2.cuda())
+    # labels = Variable(labels.cuda())
 
