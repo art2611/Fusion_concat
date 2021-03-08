bdd0fe9b79b69319aacfbc8b6a9fa2a0aba342fc
art2611 <art261197@hotmail.fr>
2021-03-08 11:15:56 +0100

    Commit

--- utils.py
+++ utils.py
@@ -80,7 +80,7 @@ class IdentityFeatureSampler(Sampler):
         print(f"index2 : {index2}")
         print(f"index1 info: {index1[:4]}")
         print(f"index2 info: {index2[:4]}")
-        print(f"index1 info: {index1[0]")
+        print(f"index1 info: {index1[0]}")
         print(f"index2 info: {index2[3]}")
         self.index1 = index1
         self.index2 = index2
