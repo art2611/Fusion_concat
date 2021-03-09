28cd7f33668de399366b4ca5bebd237563e2472c
art2611 <art261197@hotmail.fr>
2021-03-09 14:43:12 +0100

    commit

--- test_all.sh
+++ test_all.sh
@@ -12,6 +12,7 @@ fi
 read -e -p "Enter the fuse type (sum/cat/cat_channel/none / ALL) of the trained model :" FUSE
 if [ "$FUSE" =  "ALL" ]
 then
+  echo "DO ALL"
   # In this case do test of sum cat and cat channel at all position
     for fuse in 'sum' 'cat' 'cat_channel';
   do
@@ -21,11 +22,11 @@ then
     done
   done
 else
+    echo "DO $FUSE only"
       for fusion in 'early' 'layer1' 'layer2' 'layer3' 'layer4' 'layer5' ;
     do
       python test.py --fusion=$fusion --dataset=$DATASET --reid="BtoB" --trained="BtoB" --fuse=$FUSE ;
     done
-
 fi
 
 
