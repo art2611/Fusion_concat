4573a20fb3615d9678a465bf8c4aab995fc4224c
art2611 <art261197@hotmail.fr>
2021-03-09 15:44:23 +0100

    commit

--- test_all_norm.sh
+++ test_all_norm.sh
@@ -5,7 +5,7 @@ echo $DATASET
 
 for i in 'score' 'fc';
 do
-  for j in 'l2norm' 'minmax' 'tanh' 'zscore';
+  for j in 'l2norm' 'minmax' 'tanh' 'zscore' 'none';
   do
               echo "testing $j norm for $i fusion on $DATASET dataset"
               python test.py --fusion=$i --dataset=$DATASET --reid="BtoB" --trained="BtoB" --fuse="none" --norm=$j ;
