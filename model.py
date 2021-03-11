c765b3cf62496f1a4e3af5dba36dac88d3069110
art2611 <art261197@hotmail.fr>
2021-03-11 17:54:13 +0100

    commit

--- model.py
+++ model.py
@@ -180,7 +180,7 @@ class Global_network(nn.Module):
         self.bottleneck2 = nn.BatchNorm1d(pool_dim)
         self.bottleneck2.bias.requires_grad_(False)  # no shift
         self.gmu = GatedBimodal(pool_dim)
-        self.fc_fuse = nn.Sequential(nn.Linear(2*pool_dim, pool_dim, bias = False), nn.ReLU())
+        self.fc_fuse = nn.Sequential(nn.Linear(2*pool_dim, pool_dim, bias = True), nn.ReLU())
 
         self.fc = nn.Linear(pool_dim, class_num, bias=False)
         self.l2norm = Normalize(2)
