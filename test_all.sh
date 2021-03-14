a3afc0dbd3b5a5733c9bbced452d353b13e2860b
art2611 <art261197@hotmail.fr>
2021-03-14 21:31:08 +0100

    commit

--- test_all.sh
+++ test_all.sh
@@ -9,6 +9,9 @@ then
   DATASET="SYSU"
 fi
 
+read -e -p "Enter the the LOO needed (Query / Gallery) :" LOO
+echo $LOO
+
 read -e -p "Enter the fuse type (sum/cat/cat_channel/none / ALL) of the trained model :" FUSE
 if [ "$FUSE" =  "ALL" ]
 then
@@ -16,20 +19,20 @@ then
   # In this case do test of sum cat and cat channel at all position
   for reid in 'VtoV' 'TtoT';
   do
-    python test.py --fusion='unimodal' --dataset=$DATASET --reid=$reid --trained=$reid --fuse='none' ;
+    python test.py --fusion='unimodal' --dataset=$DATASET --reid=$reid --trained=$reid --fuse='none' --LOO=$LOO ;
   done
     for fuse in 'sum' 'cat' 'cat_channel';
   do
     for fusion in 'early' 'layer1' 'layer2' 'layer3' 'layer4' 'layer5' ;
     do
-      python test.py --fusion=$fusion --dataset=$DATASET --reid="BtoB" --trained="BtoB" --fuse=$fuse ;
+      python test.py --fusion=$fusion --dataset=$DATASET --reid="BtoB" --trained="BtoB" --fuse=$fuse --LOO=$LOO ;
     done
   done
 else
     echo "DO $FUSE only"
       for fusion in 'early' 'layer1' 'layer2' 'layer3' 'layer4' 'layer5' ;
     do
-      python test.py --fusion=$fusion --dataset=$DATASET --reid="BtoB" --trained="BtoB" --fuse=$FUSE ;
+      python test.py --fusion=$fusion --dataset=$DATASET --reid="BtoB" --trained="BtoB" --fuse=$FUSE --LOO=$LOO ;
     done
 fi
 
