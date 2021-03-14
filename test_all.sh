50e20e70c5b5c4451d4afa3886ba103d3459317e
art2611 <art261197@hotmail.fr>
2021-03-14 21:38:58 +0100

    commit

--- test_all.sh
+++ test_all.sh
@@ -21,7 +21,7 @@ then
 #  do
 #    python test.py --fusion='unimodal' --dataset=$DATASET --reid=$reid --trained=$reid --fuse='none' --LOO=$LOO ;
 #  done
-    for fuse in 'cat_channel';
+    for fuse in 'sum' 'cat' 'cat_channel';
   do
     for fusion in 'early' 'layer1' 'layer2' 'layer3' 'layer4' 'layer5' ;
     do
