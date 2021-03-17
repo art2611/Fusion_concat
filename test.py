159415f03bf3f854d2d602e50b9acbb527bb27d7
art2611 <art261197@hotmail.fr>
2021-03-17 17:28:03 +0100

    commit

--- test.py
+++ test.py
@@ -381,7 +381,7 @@ if True:
 
                     # distmat, distmat2 = Normalize_func(distmat, distmat2, args.norm)
                     print(f"Weight value : {args.weight}")
-                    distmat = (float(args.weight)*distmat + float(1-args.weight) * distmat2)
+                    distmat = (float(args.weight)*distmat + (1-float(args.weight)) * distmat2)
                 elif args.fusion == "fc":
                     # Proceed to a simple feature aggregation, features incoming from the two distinct unimodal trained models (RGB and IR )
                     #First do a norm :
